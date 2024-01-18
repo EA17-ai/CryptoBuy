2cc4d9eaf243bf8cd4d0f07e8a748f0ec9b03522
EA17-ai <89613171+EA17-ai@users.noreply.github.com>
2024-01-18 02:41:27 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     
   
-  <BrowserRouter basename="/crpytoBuy">
+  <BrowserRouter>
   <App />
   </BrowserRouter>
   </React.StrictMode>,
